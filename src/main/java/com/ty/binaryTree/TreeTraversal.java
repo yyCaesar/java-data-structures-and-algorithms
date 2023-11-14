d4180454265a6782fc406729de416d2a24b8b476
yyCaesar <ty.zero@outlook.com>
2023-11-14 18:59:41 +0800

    add new traversal method

--- src/main/java/com/ty/binaryTree/TreeTraversal.java
+++ src/main/java/com/ty/binaryTree/TreeTraversal.java
@@ -1,9 +1,66 @@
 package com.ty.binaryTree;
 
+import java.util.Stack;
+
 /**
  * 遍历树
  * */
 public class TreeTraversal {
+
+    /**
+     * 后序遍历（非递归遍历）
+     * */
+    public void portOrder_nonRecursion(TreeNode node){
+        TreeNode pop = null;
+        TreeNode current = node;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (current != null || !stack.isEmpty()){
+
+            if (current != null){
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode peek = stack.peek();
+                if (peek.right == null || peek.right == pop){
+                    pop = stack.pop();
+                    System.out.println(pop);
+                }else {
+                    current = peek.right;
+                }
+            }
+
+        }
+
+
+    }
+
+
+    /**
+     * 前、中序遍历（非递归遍历）
+     * */
+    public void preOrinOrder_nonRecursion(TreeNode node){
+        //栈 -> 压入、弹出数据
+        TreeNode current = node;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (current != null || !stack.isEmpty()){
+            if (current != null){
+                stack.push(current);
+//                System.out.println("前序遍历："+ current.value);
+                current = current.left;
+            } else {
+                //左边路走完了，往回走、右
+                TreeNode pop = stack.pop();
+                System.out.println("中序遍历："+ pop.value);
+                current = pop.right;
+            }
+
+        }
+
+
+
+    }
 
 
     /**
@@ -17,6 +74,33 @@ public class TreeTraversal {
         System.out.println(node.value + "\t");
         preOrder(node.left);
         preOrder(node.right);
+    }
+
+    /**
+     * 中序遍历
+     * */
+    public void inOrder(TreeNode node){
+        //左 -> 根 -> 右
+        if(node == null){
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.value + "\t");
+        inOrder(node.right);
+    }
+
+    /**
+     * 后序遍历
+     * */
+    public void postOrder(TreeNode node){
+        //左 -> 右 -> 根
+        if (node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value + "\t");
     }
 
 
--- src/test/java/com/ty/binaryTree/TestTreeTraversal.java
+++ src/test/java/com/ty/binaryTree/TestTreeTraversal.java
@@ -5,14 +5,26 @@ import org.junit.Test;
 public class TestTreeTraversal {
 
     @Test
-    public void preOrder(){
+    public void order(){
+
+        /**
+         *           1
+         *          / \
+         *        2    3
+         *       /    / \
+         *      4    5   6
+         * */
 
         TreeNode root = new TreeNode(1,
                 new TreeNode(2,new TreeNode(4),null),
                 new TreeNode(3,new TreeNode(5),new TreeNode(6)));
 
         TreeTraversal treeTraversal = new TreeTraversal();
-        treeTraversal.preOrder(root);
+//        treeTraversal.preOrder(root);  // 1 2 4 3 5 6
+//        treeTraversal.inOrder(root);   // 4 2 1 5 3 6
+//        treeTraversal.postOrder(root); // 4 2 5 6 3 1
+
+        treeTraversal.preOrinOrder_nonRecursion(root);
 
     }
 
